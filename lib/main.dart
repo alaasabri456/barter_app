d9567a0cbee6b02da69d9228df1deaf835a4e3d6
alaa <alaasabri456@gmail.com>
2026-01-06 19:53:17 +0200

    login with google

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-
+import 'firebase_options.dart';
 import 'core/prefs_manager/prefs_manager.dart';
 import 'core/routes_manager/routes_manager.dart';
 import 'core/theme/theme_provider.dart';
@@ -12,7 +12,9 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await PrefsManager.init();
 
   // Initialize push notifications
